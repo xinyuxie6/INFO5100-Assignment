e4b4ffb5ddbcfecc1cbf0da8e4df41c2021c1a78
xinyuxie6 <42994191+xinyuxie6@users.noreply.github.com>
2018-10-23 18:11:52 -0700

    Add files via upload

---
+++ Assignment6/Dog.java
@@ -0,0 +1,49 @@
+import java.util.Calendar;
+
+public class Dog extends Pet implements Boardable {
+	private String size;
+	private Calendar startDate = Calendar.getInstance();
+	private Calendar endDate = Calendar.getInstance();
+	
+	public Dog(String name, String ownerName, String color, String size) {
+		super(name, ownerName, color);
+		this.size = size;
+	}
+
+	public void check(int month, int day, int year) {
+		if(month < 1 || month > 12) {
+			throw new IllegalArgumentException("illegal month");
+		}
+		if(day < 1|| day > 31) {
+			throw new IllegalArgumentException("illegal day");
+		}
+	}
+	
+	public void setBoardStart(int month, int day, int year) {
+		check(month, day, year);
+		this.startDate.set(month, day, year);
+	}
+	
+	public void setBoardEnd(int month, int day, int year) {
+		check(month, day, year);
+		this.endDate.set(month, day, year);
+	}
+	
+	public boolean boarding(int month, int day, int year) {
+		check(month, day, year);
+		Calendar date = Calendar.getInstance();
+		date.set(month, day, year);
+		
+		return date.after(startDate) && !date.after(endDate);
+		
+	}
+	
+	public String getSize() {
+		return size;
+	}
+	
+	public String toString() {
+		return "DOG:\n" + super.toString() + "Size: " + size;
+	}
+
+}
---
+++ Assignment6/Mreview.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class Mreview implements Comparable<Mreview> {
+	private String title;
+	private ArrayList<Integer> ratings;
+	
+	public Mreview() {
+		this.title = "";
+		this.ratings = new ArrayList<>();
+	}
+	
+	public Mreview(String ttl) {
+		this.title = ttl;
+		this.ratings = new ArrayList<>();
+		
+	}
+	
+	public Mreview(String ttl, int firstRating) {
+		this.title = ttl;
+		this.ratings = new ArrayList<>();
+		this.ratings.add(firstRating);
+	}
+	
+	
+	public String getTitle() {
+		return title;
+		
+	}
+	
+	public void addRating(int r) {
+		this.ratings.add(r);
+	}
+	
+	public double aveRating() {
+		double sum = 0;
+		
+		for(int i = 0; i < ratings.size(); i++) {
+			sum += ratings.get(i);
+		}
+		return sum / ratings.size();
+	}
+
+	
+	public int numRatings() {
+		return ratings.size();
+	}
+	
+	public int CompareTo(Mreview obj) {
+		return this.title.compareTo(obj.title);
+	}
+	
+	public boolean equals(Object obj) {
+		if(this.title.equals(((Mreview) obj).title)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	public String toString() {
+		return title + ", average " + aveRating() + "out of " + numRatings();
+	}
+	
+	public static void main(String[] args) {
+		Mreview mreview = new Mreview("Kill Bill", 3);
+		mreview.addRating(4);
+		mreview.addRating(3);
+		System.out.println(mreview);
+
+	}
+	
+}
