149b7a70803ab99b9dc62db73c121fb8fdb84a07
xinyuxie6 <42994191+xinyuxie6@users.noreply.github.com>
2018-10-23 17:57:39 -0700

    Delete Boardable.java

--- Assignment6/Boardable.java
+++
@@ -1,7 +0,0 @@
-
-public interface Boardable {
-	public void setBoardStart(int month, int day, int year); 
-	public void setBoardEnd(int month, int day, int year); 
-	public boolean boarding(int month, int day, int year); 
-
-}
